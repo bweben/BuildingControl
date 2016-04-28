1df5509b2638fe0c5b0498f4ac281808d545f898
Nathanael Weber <nathanael.weber@bbcag.ch>
2016-04-28 10:02:36 +0200

    jldasjfk

--- src/com/java/interfaces/Observer.java
+++ src/com/java/interfaces/Observer.java
@@ -1,9 +1,5 @@
 package com.java.interfaces;
 
-import com.java.model.Game;
-
-import java.util.Objects;
-
 /**
  * Created by Nathanael on 24.03.2016.
  * For the change in the buildings arraylist to notify the BuildingsPanel.
